fc67972089d794906512eee060cc08666fa2fe34
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-04-03 15:30:13 +0700

    fix: remove unnecessary div

--- resources/js/Components/CrudTable.tsx
+++ resources/js/Components/CrudTable.tsx
@@ -96,124 +96,122 @@ export function CrudTable<TData extends { id: number }>({
                 </div>
             </div>
 
-            <div className="border-t border-b">
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {showIndexColumn && (
-                                    <TableHead className="w-8">No.</TableHead>
-                                )}
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead
-                                            key={header.id}
-                                            style={{
-                                                width: header.column.getSize(),
-                                            }}
-                                        >
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef
-                                                        .header,
-                                                    header.getContext()
-                                                )}
-                                        </TableHead>
-                                    );
-                                })}
-                                {showActionColumn && (
+            <Table className="border-t border-b">
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {showIndexColumn && (
+                                <TableHead className="w-8">No.</TableHead>
+                            )}
+                            {headerGroup.headers.map((header) => {
+                                return (
                                     <TableHead
-                                        style={{ width: 40 }}
-                                        className="text-center"
+                                        key={header.id}
+                                        style={{
+                                            width: header.column.getSize(),
+                                        }}
                                     >
-                                        <Button
-                                            variant="ghost"
-                                            size="sm"
-                                            onClick={() => refreshData()}
-                                        >
-                                            <RefreshCcw />
-                                            <span className="sr-only">
-                                                Refresh
-                                            </span>
-                                        </Button>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef
+                                                    .header,
+                                                header.getContext()
+                                            )}
                                     </TableHead>
+                                );
+                            })}
+                            {showActionColumn && (
+                                <TableHead
+                                    style={{ width: 40 }}
+                                    className="text-center"
+                                >
+                                    <Button
+                                        variant="ghost"
+                                        size="sm"
+                                        onClick={() => refreshData()}
+                                    >
+                                        <RefreshCcw />
+                                        <span className="sr-only">
+                                            Refresh
+                                        </span>
+                                    </Button>
+                                </TableHead>
+                            )}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={
+                                    row.getIsSelected() && "selected"
+                                }
+                            >
+                                {showIndexColumn && (
+                                    <TableCell>
+                                        {(dataQuery.data?.from ?? 1) +
+                                            row.index}
+                                        .
+                                    </TableCell>
+                                )}
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </TableCell>
+                                ))}
+
+                                {showActionColumn && (
+                                    <TableCell className="text-center">
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button
+                                                    variant="ghost"
+                                                    className="h-8 w-8 p-0"
+                                                    size="sm"
+                                                >
+                                                    <span className="sr-only">
+                                                        Open menu
+                                                    </span>
+                                                    <MoreHorizontal className="h-4 w-4" />
+                                                </Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent align="end">
+                                                <DropdownMenuItem
+                                                    onClick={() => {
+                                                        form.form.reset(row.original)
+                                                        setShowForm(true)
+                                                    }
+                                                    }
+                                                >
+                                                    <Edit /> Edit
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem onClick={() => handleDelete(row.original.id)} >
+                                                    <Trash2 /> Delete
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    </TableCell>
                                 )}
                             </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={
-                                        row.getIsSelected() && "selected"
-                                    }
-                                >
-                                    {showIndexColumn && (
-                                        <TableCell>
-                                            {(dataQuery.data?.from ?? 1) +
-                                                row.index}
-                                            .
-                                        </TableCell>
-                                    )}
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
-                                        </TableCell>
-                                    ))}
-
-                                    {showActionColumn && (
-                                        <TableCell className="text-center">
-                                            <DropdownMenu>
-                                                <DropdownMenuTrigger asChild>
-                                                    <Button
-                                                        variant="ghost"
-                                                        className="h-8 w-8 p-0"
-                                                        size="sm"
-                                                    >
-                                                        <span className="sr-only">
-                                                            Open menu
-                                                        </span>
-                                                        <MoreHorizontal className="h-4 w-4" />
-                                                    </Button>
-                                                </DropdownMenuTrigger>
-                                                <DropdownMenuContent align="end">
-                                                    <DropdownMenuItem
-                                                        onClick={() => {
-                                                            form.form.reset(row.original)
-                                                            setShowForm(true)
-                                                        }
-                                                        }
-                                                    >
-                                                        <Edit /> Edit
-                                                    </DropdownMenuItem>
-                                                    <DropdownMenuItem onClick={() => handleDelete(row.original.id)} >
-                                                        <Trash2 /> Delete
-                                                    </DropdownMenuItem>
-                                                </DropdownMenuContent>
-                                            </DropdownMenu>
-                                        </TableCell>
-                                    )}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className="h-24 text-center"
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div >
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell
+                                colSpan={columns.length}
+                                className="h-24 text-center"
+                            >
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
 
             <DataTablePagination table={table} data={dataQuery.data} />
 
